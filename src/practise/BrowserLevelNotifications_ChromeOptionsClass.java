9e0ec6150f5e5895da9ffed0c8bf35d458d42168
admin <admin@DESKTOP-CMQ45V3>
2020-02-02 23:37:49 +0530

    commit 02-02-2020

--- src/practise/BrowserLevelNotifications_ChromeOptionsClass.java
+++ src/practise/BrowserLevelNotifications_ChromeOptionsClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class BrowserLevelNotifications {
+public class BrowserLevelNotifications_ChromeOptionsClass {
 
 	public static void main(String[] args) {
 	
@@ -17,7 +17,17 @@ public class BrowserLevelNotifications {
 		WebDriver driver =new ChromeDriver(options);
 		driver.get("https://www.google.com");
 		
-
+		
+	/*	start-maximized: Opens Chrome in maximize mode
+		incognito: Opens Chrome in incognito mode
+		headless: Opens Chrome in headless mode
+		disable-extensions: Disables existing extensions on Chrome browser
+		disable-popup-blocking: Disables pop-ups displayed on Chrome browser
+		make-default-browser: Makes Chrome default browser
+		version: Prints chrome browser version
+		disable-infobars: Prevents Chrome from displaying the notification 'Chrome is being controlled by automated software
+	*/
+		
 	}
 
 }
